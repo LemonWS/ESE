dcb2cec1703461ba58ad06c2837bedd513eecf28
LemonWS <129055047+LemonWS@users.noreply.github.com>
2024-04-12 21:56:07 +1000

    Add files via upload

---
+++ code/LTraining.py
@@ -0,0 +1,34 @@
+# this part is to calculate the equilibrium parameter adjustment matrix
+from itertools import count
+#,
+def L_parameters(state, state_p, n=10):
+    l = []
+    if n is None:
+        for i in count():
+            l1 = (state[i] - state_p[i] + l)/2
+            if l1 == l:
+                return l
+            else:
+                l = l1
+                return l
+
+    elif isinstance(n, (int, float)):
+        for i in range(n):
+            l = (state[i] - state_p[i] + l) / 2
+        return l
+
+
+
+
+
+
+    #for i in range(n)
+
+    return l
+
+print(L_parameters(n=4))
+
+
+
+
+
