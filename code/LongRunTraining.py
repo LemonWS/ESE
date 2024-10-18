18d96ee27f77fa08975d4a660fa8ccfdbf2ad808
LemonWS <129055047+LemonWS@users.noreply.github.com>
2024-10-18 20:31:31 +1100

    Update LongRunTraining.py

--- code/LongRunTraining.py
+++ code/LongRunTraining.py
@@ -6,48 +6,49 @@ from Cointegration import cointegration
 from EquilibriumParameter import equilibrium_state_parameter_set
 
 
-#,
-def L_parameters(esps, state):
-    no_part = len(esps)
-    #print(no_part)
-    l = np.ones(no_part)
-    #print(no_part)
-    for i in range(len(state)):
-        l = (esps - state[i] + l)/2
-        #print(l1)
-    return l
+### Version 1 - Not in use
+# def L_parameters(esps, state):
+#     no_part = len(esps)
+#     #print(no_part)
+#     l = np.ones(no_part)
+#     #print(no_part)
+#     for i in range(len(state)):
+#         l = (esps - state[i] + l)/2
+#         #print(l1)
+#     return l
 
- #   elif isinstance(n, (int, float)):
-  #      for i in range(n):
- #           l = (state[i] - state_p[i] + l) / 2
-#        return l
-
-
-#print(L_parameters(n=4))
+#  #   elif isinstance(n, (int, float)):
+#   #      for i in range(n):
+#  #           l = (state[i] - state_p[i] + l) / 2
+# #        return l
 
 
-def long_run_equilibrium(esps_0,spss):
-    esps = esps_0
-    i = 0
-    no_part = len(esps)
-    l = np.ones(no_part)
+# print(L_parameters(n=4))
 
-    while cointegration(esps,spss):
+### Version 2 - Not in use
+# def long_run_equilibrium(esps_0,spss):
+#     esps = esps_0
+#     i = 0
+#     no_part = len(esps)
+#     l = np.ones(no_part)
 
-        l = (esps - spss[i] + l) / 2
+#     while cointegration(esps,spss):
 
-        esps = (esps_0 + l) / 2
+#         l = (esps - spss[i] + l) / 2
 
-        i += 1
+#         esps = (esps_0 + l) / 2
 
-    return esps
+#         i += 1
 
+#     return esps
+
+### long run equilibrium training of esps with parameter l
 def long_run_equilibrium_l(esps_0,spss):
 
     #print(esps)
     i = 0
-    no_part = len(esps_0)
-    l = np.ones(no_part)
+    no_part = len(esps_0)  ### number of parts, esps_0 as the initial esps
+    l = np.ones(no_part)   ### initialise all values of vector l to 1
 
     while True:
         for i in range(len(spss)):
@@ -56,7 +57,7 @@ def long_run_equilibrium_l(esps_0,spss):
         esps = esps_0 - (l / 2)
 
 
-        if cointegration(esps,spss) == False:
+        if cointegration(esps,spss) == False:    ### break if esps and spss are in cointegration! Note: False
             break
 
     return esps
