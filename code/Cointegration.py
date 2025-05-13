741a3cf9d99d1c7af9bac4f0800825cae139ca04
LemonWS <129055047+LemonWS@users.noreply.github.com>
2025-05-13 16:00:38 +1000

    Update Cointegration.py

--- code/Cointegration.py
+++ code/Cointegration.py
@@ -6,23 +6,24 @@ import pandas as pd
 from PredictTest import pvalue
 
 
-def cointegration(ES,S):
+ef cointegration(ES, S):
     r = False
     p = 0.05
     length = S.shape[0]
     ES = np.tile(ES, (length, 1))
-    
+
     for i in range(len(S)):
-        test = ts.coint(ES[i,:],S[i,:])
+        test = ts.coint(ES[i, :], S[i, :])
         if test == None:
             test = 0
-        #print(test[1])
+            r = True
+            break
+        # print(test[1])
 
         if test[1] > p:
             r = True
-            #print(r)
+            # print(r)
             break
-
 
     return r
 
